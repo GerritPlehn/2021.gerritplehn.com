6ddec1a7f22be88b167620c4e244bf6d9a832c7f
Gerrit Plehn <gerritplehn@gmail.com>
2021-08-02 17:21:57 +0200

    initial commit

---
+++ components/Article.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    v-editable="blok"
+    class="prose my-24 mx-auto">
+    <h1>{{ blok.name }}</h1>
+    <p>{{ blok.intro }}</p>
+    <div class="prose" v-dompurify-html="$md.render(blok.body)"></div>
+  </div>
+</template>
+ 
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <header class="max-w-5xl mx-auto py-8 flex">
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link
+            class="text-teal-600 hover:underline"
+            to="/">
+            Home
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link
+            class="text-teal-600 hover:underline"
+            to="/articles">
+            Blog
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link
+            class="text-teal-600 hover:underline"
+            to="/contact">
+            Contact
+          </nuxt-link>
+        </li>
+      </ul>
+    </nav>
+    <div class="flex-1">
+      <a href="/" class="block w-56 mx-auto">
+        <img src="https://a.storyblok.com/f/121715/x/496734270f/gp_logo.svg">
+      </a>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link
+            class="text-teal-600 hover:underline"
+            to="/articles">
+            English
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link
+            class="text-teal-600 hover:underline"
+            to="/de/articles">
+            German
+          </nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
