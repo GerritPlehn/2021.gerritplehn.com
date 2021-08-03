3d94a1b85eb148d92b5f837a09f4f4df04e65da1
Gerrit Plehn <gerritplehn@gmail.com>
2021-08-03 12:12:12 +0200

    header links to /

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,12 @@
 <template>
   <header class="text-gray-400 body-font">
   <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a class="flex title-font font-medium items-center text-gray-900 dark:text-white mb-4 md:mb-0">
+    <nuxt-link to="/" class="flex title-font font-medium items-center text-gray-900 dark:text-white mb-4 md:mb-0">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-red-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg>
       <span class="ml-3 text-xl">Gerrit Plehn</span>
-    </a>
+    </nuxt-link>
     <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
       <nuxt-link class="mr-5 hover:text-white" to="/articles">Articles</nuxt-link>
 
